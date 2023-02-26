f26905ed162dd78cb5a19d545e51f28c18f2854b
othmanben001 <othman@hismarcv.com>
2023-02-26 19:40:39 +0100

    Setup google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics measurementId="G-DPBNLCDD0V" />
+      <Component {...pageProps} />
+    </>
+  );
 }
