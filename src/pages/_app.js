07ba803710ea0e974abb8aacbf3cde5953749692
othmanben001 <othman@hismarcv.com>
2023-03-08 22:37:40 +0100

    Adding Vercel Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import GoogleAnalytics from "@bradgarropy/next-google-analytics";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GoogleAnalytics measurementId="G-DPBNLCDD0V" />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
