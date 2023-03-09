4ff6bdd2e238ac6c9e0a8bda2bd88b664ff43b30
othmanben001 <othman@hismarcv.com>
2023-03-09 20:27:12 +0100

    adding hotjar manual snippets

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,6 +32,16 @@ export default function Document() {
           gtag('config', '${GMT_ID}');
         `}
         </Script>
+        <Script strategy="afterInteractive">
+          {`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3400590,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+        </Script>
         {/* Google tag (gtag.js) end */}
         {/* <Script
           dangerouslySetInnerHTML={{
