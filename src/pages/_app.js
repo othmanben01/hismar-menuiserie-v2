abd05b23693d4eed008cca424ad1f477501e75b9
othmanben001 <othman@hismarcv.com>
2023-03-09 15:01:44 +0100

    moving google analytics snippets to google tag manager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,12 @@
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
-import GoogleAnalytics from "@bradgarropy/next-google-analytics";
+// import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GoogleAnalytics measurementId="G-DPBNLCDD0V" />
+      {/* <GoogleAnalytics measurementId="G-DPBNLCDD0V" /> */}
       <Component {...pageProps} />
       <Analytics />
     </>
